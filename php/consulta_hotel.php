3dd846c7cb95973c8d5d7ab52b85b82aa4a98813
soujiro27 <jc.raus27@gmail.com>
2013-11-20 08:59:54 -0600

    casi terminada la consulta de hotele

--- php/consulta_hotel.php
+++ php/consulta_hotel.php
@@ -11,35 +11,35 @@ if ($row = $db->fetch_array($consulta))
 {
 	$estrellas=$row['categoria'];
    do{
-   	$html="<article id=img_hotel><figure id=imagen_hotel><img src=".$row['foto']."></figure><article id=categoria>";
+   	$html="<section id=cuadro_foto><article id=img_hotel><figure id=imagen_hotel><img src=".$row['foto']."></figure><article id=categoria>";
    	
      while($estrellas>0)
      {
      	$html=$html."<figure><img src=img/estrella.png></figure>";
      	$estrellas=$estrellas-1;
      }
-     $html=$html."</article>";
-     $html=$html."<article id=servicios>";
+     $html=$html."</article></section>";
+     $html=$html."<section id=cuadro_servicios><article id=servicios>";
      if($row['restaurant']>0)
      {
-     	$html=$html."<figure><img src=img/resta.png></figure>";
+     	$html=$html."<figure><img src=img/resta.png><figcaption>Restaurante</figcaption></figure>";
      }
 
      if($row['alberca']>0)
      {
-     	$html=$html."<figure><img src=img/alberca.png></figure>";
+     	$html=$html."<figure><img src=img/alberca.png><figcaption>Alberca</figcaption></figure>";
      }
 
      if($row['gym']>0)
      {
-     	$html=$html."<figure><img src=img/gym.png></figure>";
+     	$html=$html."<figure><img src=img/gym.png><figcaption>Gimnasio</figcaption></figure>";
      }
 
      if($row['wifi']>0)
      {
-     	$html=$html."<figure><img src=img/wifi.png></figure>";
+     	$html=$html."<figure><img src=img/wifi.png><figcaption>Internet</figcaption></figure>";
      }
-     $html=$html."</article><article id=direccion><p>".$row['direccion']."</p></article><article id=mapa><figure><img src=img/morelos/mapa.gif></figure></article>";
+     $html=$html."</article><article id=descripcion><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, ipsum, iste, optio exercitationem itaque placeat fugit numquam ipsam ullam porro quo temporibus et veniam esse possimus blanditiis veritatis aliquam molestiae.</article><article id=mapa><figure><img src=img/morelos/mapa.gif></figure></article></section><article><p>".$row['direccion']."</p></article>";
 
    } while($row = $db->fetch_array($consulta)); 
  $html=$html."</article>";
